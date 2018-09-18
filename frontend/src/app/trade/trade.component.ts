0fac6e906978518cbbda47d73be5216d1d40aeac
Ilarion Golovin <ilarion.golovin@gmail.com>
2018-09-18 03:29:22 +0300

    -

--- frontend/src/app/trade/trade.component.ts
+++ frontend/src/app/trade/trade.component.ts
@@ -4,6 +4,9 @@ import {HttpClient, HttpResponse} from '@angular/common/http';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {TickerName} from "../model/ticker.name";
 import {TickerInfo} from "../model/ticker.info";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
 
 declare const TradingView: any;
 @Component({
@@ -40,60 +43,76 @@ export class TradeComponent implements  OnInit, AfterViewInit, OnChanges {
     );
   }
 
-  getTickerNames() {
-    return this.http.get<Array<TickerName>>(`http://localhost:8080/get_ticker_names`);
-  }
-
   init(exchangeFromUrl: String, currency1FromUrl: String, currency2FromUrl: String) {
     this.exchanges = [];
     this.tickersOfSelectedExchange = [];
 
-    this.getTickerNames().pipe().subscribe(data => {
-      this.tickerNames = data;
+    Observable.forkJoin(
+      this.http.get<Array<TickerName>>(`http://localhost:8080/get_ticker_names`).map((response:Array<TickerName>) => response),
+      this.http.get<TickerInfo>(`http://localhost:8080/get_ticker_info`).map((response:TickerInfo) => response)
+    ).subscribe(
+      data => {
+        this.tickerNames = data[0];
+        this.handleTickerNames(this.tickerNames, exchangeFromUrl, currency1FromUrl, currency2FromUrl);
+        this.tickerInfo = data[1];
+        new TradingView.widget({
+          'container_id': 'technical-analysis',
+          "width": '100%',
+          "height": '500',
+          'symbol': ''+this.selectedTickerCurrency1 + this.selectedTickerCurrency2,
+          'interval': '60',
+          'timezone': 'exchange',
+          'theme': 'Light',
+          'style': '1',
+          'toolbar_bg': '#f1f3f6',
+          'withdateranges': true,
+          'hide_side_toolbar': false,
+          'allow_symbol_change': true,
+          'save_image': false,
+          'hideideas': true,
+          'studies': [
+            'MASimple@tv-basicstudies' ],
+          'show_popup_button': true,
+          'popup_width': '1000',
+          'popup_height': '650'
+        });
+      },
+      error => console.error(error)
+    );
+  }
 
-      for (const t of this.tickerNames) {
-        if (this.exchanges.indexOf(t.exchange) == -1) {
-          this.exchanges.push(t.exchange);
-        }
-        console.log(`Ticker: '${t.exchange}' : '${t.currency1}' / '${t.currency2}' `);
+  handleTickerNames(tickerNames:Array<TickerName>,exchangeFromUrl: String, currency1FromUrl: String, currency2FromUrl: String) {
+    for (const t of this.tickerNames) {
+      if (this.exchanges.indexOf(t.exchange) == -1) {
+        this.exchanges.push(t.exchange);
       }
+      console.log(`Ticker: '${t.exchange}' : '${t.currency1}' / '${t.currency2}' `);
+    }
 
-      if (exchangeFromUrl != null) {
-        this.selectedExchange = exchangeFromUrl;
-      } else {
-        this.selectedExchange = this.exchanges[0];
+    if (exchangeFromUrl != null) {
+      this.selectedExchange = exchangeFromUrl;
+    } else {
+      this.selectedExchange = this.exchanges[0];
+    }
+    console.log(this.selectedExchange);
+
+    for (const t of this.tickerNames) {
+      if (t.exchange == this.selectedExchange) {
+        this.tickersOfSelectedExchange.push(t);
+        console.log("added to tickersOfSelectedExchange"+ t.currency1 + t.currency2);
       }
-      console.log(this.selectedExchange);
+    }
 
-      for (const t of this.tickerNames) {
-        if (t.exchange == this.selectedExchange) {
-          this.tickersOfSelectedExchange.push(t);
-          console.log("added to tickersOfSelectedExchange"+ t.currency1 + t.currency2);
-        }
-      }
-
-      if (currency1FromUrl != null && currency2FromUrl != null) {
-        this.selectedTickerCurrency1 = currency1FromUrl;
-        this.selectedTickerCurrency2 = currency2FromUrl;
-      } else {
-        this.selectedTickerCurrency1 = this.tickerNames[0].currency1;
-        this.selectedTickerCurrency2 = this.tickerNames[0].currency2;
-      }
-
-    });
-
-    this.http.get<TickerInfo>(`http://localhost:8080/get_ticker_info`).pipe().subscribe(data => {
-      this.tickerInfo = data;
-      console.log("get_ticker_info: " + data.price);
-    });
+    if (currency1FromUrl != null && currency2FromUrl != null) {
+      this.selectedTickerCurrency1 = currency1FromUrl;
+      this.selectedTickerCurrency2 = currency2FromUrl;
+    } else {
+      this.selectedTickerCurrency1 = this.tickerNames[0].currency1;
+      this.selectedTickerCurrency2 = this.tickerNames[0].currency2;
+    }
   }
 
   ngOnInit() {
-    let exchangeFromUrl = this.route.snapshot.paramMap.get('exchange');
-    let currency1FromUrl = this.route.snapshot.paramMap.get('currency1');
-    let currency2FromUrl = this.route.snapshot.paramMap.get('currency2');
-
-    //this.init(exchangeFromUrl, currency1FromUrl, currency2FromUrl);
   }
 
   getSelectedTicker() {
@@ -101,27 +120,8 @@ export class TradeComponent implements  OnInit, AfterViewInit, OnChanges {
   }
 
   ngAfterViewInit() {
-    new TradingView.widget({
-      'container_id': 'technical-analysis',
-      "width": '100%',
-      "height": '500',
-      'symbol': 'BTCUSD',
-      'interval': '60',
-      'timezone': 'exchange',
-      'theme': 'Light',
-      'style': '1',
-      'toolbar_bg': '#f1f3f6',
-      'withdateranges': true,
-      'hide_side_toolbar': false,
-      'allow_symbol_change': true,
-      'save_image': false,
-      'hideideas': true,
-      'studies': [
-        'MASimple@tv-basicstudies' ],
-      'show_popup_button': true,
-      'popup_width': '1000',
-      'popup_height': '650'
-    });
+    // new TradingView.widget({
+    // moved to init
   }
 
   ngOnChanges(changes: SimpleChanges): void {
