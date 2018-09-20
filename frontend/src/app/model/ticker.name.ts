a10ae6c3b83b43b97cb337cfedfad72126d6fadd
Ilarion Golovin <ilarion.golovin@gmail.com>
2018-09-20 23:06:07 +0300

    angular 5->6

--- frontend/src/app/model/ticker.name.ts
+++
@@ -1,5 +0,0 @@
-export class TickerName {
-  exchange: string;
-  currency1: string;
-  currency2: string;
-}
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  restApiUrl: "http://188.243.168.9:8081"
-};
