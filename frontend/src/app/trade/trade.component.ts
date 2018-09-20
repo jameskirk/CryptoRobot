be6674e9784898b9196ace704c659478216e3ebf
Ilarion Golovin <ilarion.golovin@gmail.com>
2018-09-21 00:57:08 +0300

    no message

--- frontend/src/app/trade/trade.component.ts
+++ frontend/src/app/trade/trade.component.ts
@@ -70,18 +70,17 @@ export class TradeComponent implements  OnInit, OnDestroy , AfterViewInit, OnCha
       "height": '500',
       'symbol': '' + this.selectedTickerCurrency1 + this.selectedTickerCurrency2,
       'interval': '60',
-      'timezone': 'exchange',
+      'timezone': 'Europe/Moscow',
       'theme': 'Light',
       'style': '1',
       'toolbar_bg': '#f1f3f6',
-      'withdateranges': true,
-      'hide_side_toolbar': false,
-      'allow_symbol_change': true,
+      //'withdateranges': true,
+      //'hide_side_toolbar': false,
       'save_image': false,
       'hideideas': true,
       'studies': [
         'MASimple@tv-basicstudies'],
-      'show_popup_button': true,
+      //'show_popup_button': true,
       'popup_width': '1000',
       'popup_height': '650'
     });
